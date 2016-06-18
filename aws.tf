3a1101a8d07b289c6264c837017b771fbeb705fc
Timothy Washington <twashing@interruptsoftware.com>
2016-06-18 14:54:55 -0700

    Adding some artifacts to the AWS terraform config, in order to attach my image to a running EC2 instance "aws_iam_role" "aws_iam_policy" "aws_iam_policy_attachment" "aws_iam_instance_profile" "aws_security_group"

--- aws.tf
+++ aws.tf
@@ -28,8 +28,98 @@ resource "aws_ecs_task_definition" "ednonlineeditor-task" {
   }
 }
 
+resource "aws_iam_role" "ednonlineeditor" {
+    name = "ednonlineeditor"
+    assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ednonlineeditor" {
+    name = "ednonlineeditor"
+    path = "/"
+    description = "ednonlineeditor"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:Submit*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "ednonlineeditor" {
+    name = "ednonlineeditor"
+    roles = ["${aws_iam_role.ednonlineeditor.name}"]
+    policy_arn = "${aws_iam_policy.ednonlineeditor.arn}"
+}
+
+resource "aws_iam_instance_profile" "ednonlineeditor" {
+    name = "ednonlineeditor"
+    roles = ["${aws_iam_role.ednonlineeditor.name}"]
+}
+
+resource "aws_security_group" "ednonlineeditor" {
+  name = "ednonlineeditor"
+  description = "ednonlineeditor"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "edneditoronline-instance" {
   ami = "ami-bb473cdb"
-  instance_type = "t2.micro"
-  user_data = "#!/bin/bash \n echo ECS_CLUSTER=ednonlineeditor >> /etc/ecs/ecs.config"
+  instance_type = "t2.small"
+   security_groups = ["${aws_security_group.ednonlineeditor.name}"]
+  iam_instance_profile = "ednonlineeditor"
+  user_data = "#!/bin/bash\necho ECS_CLUSTER=ednonlineeditor >> /etc/ecs/ecs.config"
 }
